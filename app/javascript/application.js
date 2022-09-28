63cef96f1945a7cea6664c58dae51e73dc5e3afa
bbasanta238 <bbasanta238@gmail.com>
2022-09-28 17:10:05 +0545

    integrating metamask and web3

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,17 @@
 // Entry point for the build script in your package.json
+// import { Turbo } from "@hotwired/turbo-rails"
+// Turbo.session.drive = false
+
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 
+// js library
 import "./src/jquery"
-$(function(){
-    console.log("From Jquery")
-})
+import "./src/web3"
+
+// custom js
+import "./customjs/index"
+
+
+
